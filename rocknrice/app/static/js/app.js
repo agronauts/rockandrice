ea6cf4995488f5cd150f20d139f31de65983ed9d
Patrick Nicholls <padawagon@gmail.com>
2017-04-12 19:29:24 +1200

    Separate climb and crag views as tabs

--- rocknrice/app/static/js/app.js
+++ rocknrice/app/static/js/app.js
@@ -3,14 +3,13 @@ let ReactDOM = require('react-dom');
 let $ = require('jquery');
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
-//TODO Abstract crag/climb
 let SearchForm = React.createClass({
     searchChange: function(event) {
         this.props.onSearchRequest(event.target.value);
     },
     render: function () {
         return (
-            <div className="content">
+            <div>
                 <div className="form">
                     <div>
                         <label>Name:</label>
@@ -23,6 +22,29 @@ let SearchForm = React.createClass({
 });
 
 
+let CragList = React.createClass({
+    render: function () {
+        let crags = this.props.data.map(function (crag) {
+            return (
+                <tr>
+                    <td>{crag.name}</td>
+                    <td>{crag.description.substring(0,30)}</td>
+                </tr>
+            );
+        });
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                </tr>
+                </thead>
+                <tbody>{crags}</tbody>
+            </table>
+        )
+    }
+});
 let ClimbList = React.createClass({
     render: function () {
         let climbs = this.props.data.map(function (climb) {
@@ -48,6 +70,41 @@ let ClimbList = React.createClass({
 });
 
 let ClimbSearch = React.createClass({
+    getInitialState: function () {
+        return {
+            all_climbs: [],
+            cur_climb: []
+        }
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: '../static/climbs.json',
+            dataType: 'json',
+            success: function(data) {
+                this.setState({all_climbs: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    filterClimbs: function (query) {
+        let filtered_climbs = this.state.all_climbs.filter(function (climb) {
+            return climb.name.toLowerCase().includes(query);
+        });
+        this.setState({cur_climb: filtered_climbs});
+    },
+    render: function () {
+        return (
+            <div>
+                <h2>Climb Search</h2>
+                <SearchForm onSearchRequest={this.filterClimbs}/>
+                <ClimbList data={this.state.cur_climb}/>
+            </div>
+        );
+    }
+});
+let CragSearch = React.createClass({
     getInitialState: function () {
        return {
            all_crags: [],
@@ -75,7 +132,7 @@ let ClimbSearch = React.createClass({
     render: function () {
         return (
             <div>
-                <p>Product Search</p>
+                <p>Crag Search</p>
                 <SearchForm onSearchRequest={this.filterCrags}/>
                 <ClimbList data={this.state.cur_crags}/>
             </div>
@@ -85,26 +142,18 @@ let ClimbSearch = React.createClass({
 
 
 let App = React.createClass({
-    handleSelect: function(index, last) {
-        console.log('Selected tab: ' + index + ', Last tab: ' + last);
-    },
-
     render: function () {
         return (
-            <Tabs onSelect={this.handleSelect} selectedIndex={2}>
+            <Tabs selectedIndex={0}>
                 <TabList>
-                  <Tab>Foo</Tab>
-                  <Tab>Bar</Tab>
-                  <Tab>Baz</Tab>
+                  <Tab>Crags</Tab>
+                  <Tab>Climbs</Tab>
                 </TabList>
                 <TabPanel>
-                  <h2><ClimbSearch/></h2>
+                  <CragSearch/>
                 </TabPanel>
                 <TabPanel>
-                  <h2>Hello from Bar</h2>
-                </TabPanel>
-                <TabPanel>
-                  <h2>Hello from Baz</h2>
+                  <ClimbSearch/>
                 </TabPanel>
             </Tabs>
     );
