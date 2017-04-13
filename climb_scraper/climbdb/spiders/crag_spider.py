a8c2f52c4df7aefc33390d75f3dce6f66fd7f46d
Patrick Nicholls <padawagon@gmail.com>
2017-04-13 20:16:43 +1200

    Consider only route and nonroute pages

--- climb_scraper/climbdb/spiders/crag_spider.py
+++ climb_scraper/climbdb/spiders/crag_spider.py
@@ -15,20 +15,36 @@ class ClimbSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://climbnz.org.nz/nz/si/canterbury/port-hills/ampitheatre-wall/',
+            'http://climbnz.org.nz/nz/si/canterbury/port-hills/cattlestop-crag/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
-        for href in response.xpath('//tr/td/div/a/@href').extract():
-            yield scrapy.Request(response.urljoin(href),
-                                 callback=self.parse_climb)
+    def find_route_links(self, response):
+        return response.xpath('//tr/td/div/a/@href').extract()
 
-    def parse_climb(self, response):
+    def find_nonroute_links(self, response):
+        return response.xpath('//div/table/tbody/tr/td/a/@href').extract()
+
+    def parse(self, response):
+        return self.parse_nonroute_page(response)
+
+    def parse_nonroute_page(self, response):
+        # Identify pages that are walls
+        for href in self.find_nonroute_links(response):
+            yield scrapy.Request(response.urljoin(href),
+                                 callback=self.parse_nonroute_page)
+
+        # Identify pages that are routes
+        for href in self.find_route_links(response):
+            yield scrapy.Request(response.urljoin(href),
+                                 callback=self.parse_route_page)
+
+    def parse_route_page(self, response):
         l = ClimbLoader(item=ClimbItem(), response=response)
         l.add_xpath('name', '//h1/text()')
         l.add_xpath('description', '//div/p/text()')
+        # TODO Climbing instructions as well as description
         return l.load_item()
 
 class CragSpider(scrapy.Spider):
